d5eda00cdeb2a6ff2ae75688590783ea2c760034
dmpierre <pdaixmoreux@gmail.com>
2023-07-16 15:46:47 +0200

    feat: tornado deployment working

--- script/ETHTornado.s.sol
+++ script/ETHTornado.s.sol
@@ -10,12 +10,13 @@ import "src/MerkleTreeWithHistory.sol";
 contract ETHTornadoScript is Script {
     function setUp() public {}
 
+    IVerifier verifier = IVerifier(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
+    IHasher hasher = IHasher(0x5FbDB2315678afecb367f032d93F642f64180aa3);
+
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Verifier verifier = new Verifier();
-        MiMC mimc = new MiMC();
-        new ETHTornado(verifier, mimc, 0.05 ether, 20);
+        ETHTornado tornado = new ETHTornado(verifier, hasher, 0.05 ether, 20);
         vm.stopBroadcast();
     }
 }
