77ba271703caec8af809bbd78eeeefc3ce8dcee8
dmpierre <pdaixmoreux@gmail.com>
2023-07-16 21:43:06 +0200

    test: init and update remappings

---
+++ test/OuraganTest.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "forge-std/Test.sol";
+
+contract OuraganTest is Test {
+
+    function setUp() public {
+    }
+
+    function testValidRoot() public {
+    }
+
+    function testInvalidRoot() public {
+    }
+}
