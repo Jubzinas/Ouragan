6f39ca9f8cd009ed7c8efa1daeb4176778157c50
dmpierre <pdaixmoreux@gmail.com>
2023-07-16 14:34:02 +0200

    feat: add MiMC

--- script/ETHTornado.s.sol
+++ script/ETHTornado.s.sol
@@ -4,16 +4,18 @@ pragma solidity ^0.7.0;
 import "forge-std/Script.sol";
 import "src/ETHTornado.sol";
 import "src/Verifier.sol";
+import "src/MiMC.sol";
 import "src/MerkleTreeWithHistory.sol";
 
 contract ETHTornadoScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
         Verifier verifier = new Verifier();
-        // deploy mimc contract hasher
-        new ETHTornado(verifier, hasher, 0.05, 20);
+        MiMC mimc = new MiMC();
+        new ETHTornado(verifier, mimc, 0.05 ether, 20);
         vm.stopBroadcast();
     }
 }
